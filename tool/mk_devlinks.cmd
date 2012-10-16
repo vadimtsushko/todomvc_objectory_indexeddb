38f7b4b3838b2a2dbd0da45f6cf15dde076e137d
Vadim Tsushko <vadimtsushko@gmail.com>
2012-10-16 19:08:29 +0600

    Apparently all works in Dartium and dart2js (on Canary Chrome)

---
+++ tool/mk_devlinks.cmd
@@ -0,0 +1,6 @@
+cd ..\packages\
+rmdir objectory
+mklink /J objectory c:\projects\objectory\lib
+rmdir lawndart
+mklink /J lawndart c:\projects\lawndart\lib
+cd ..\bin
--- web/model.dart
+++ web/model.dart
@@ -3,7 +3,8 @@
 // BSD-style license that can be found in the LICENSE file.
 
 library model;
-
+import 'package:objectory/objectory_indexeddb.dart';
+import 'package:web_components/watcher.dart';
 class MainComponent {
   MainComponent();
 
@@ -27,25 +28,45 @@ MainComponent get viewModel {
 
 // The real model:
 
-class App {
-  List<Todo> todos;
-
+class App {  
+  List todos;
+  resetTodos(List value) {
+    todos = value;
+    dispatch();
+  }  
   App() : todos = <Todo>[];
 }
 
-class Todo {
-  String task;
-  bool done = false;
+class Todo extends PersistentObject {
+  
+  String get dbType() => 'Todo';
+  
+  String get task() => getProperty('task');
+  set task(String value) => setProperty('task',value);  
 
-  Todo(this.task);
-
-  String toString() => "$task ${done ? '(done)' : '(not done)'}";
+  bool get done() => getProperty('done');
+  set done(bool value) => setProperty('done',value);        
+  
+  Todo(String newTask) {
+    done = false;
+    task = newTask;
+    saveOnUpdate = true;
+  }
+  //String toString() => "$task ${done ? '(done)' : '(not done)'}";
 }
+ObjectoryQueryBuilder get $Todo => new ObjectoryQueryBuilder('Todo');
 
 App _app;
 App get app {
   if (_app == null) {
     _app = new App();
+     objectory = new ObjectoryLawndartImpl('TodoMVC_Objectory', () => 
+         objectory.registerClass('Todo',()=>new Todo('')));
+     objectory.initDomainModel().then((_) {
+       objectory.find($Todo).then((todos) {
+         app.resetTodos(todos);         
+       }); 
+     });
   }
   return _app;
 }
